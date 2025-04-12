448e79f96a112320a629daf33954cb281b9c9425
Nikhil Sarin <77852027+nikhilsarin1@users.noreply.github.com>
2025-04-12 15:55:01 -0400

    Initial Commit

---
+++ RightNow/RightNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  RightNowApp.swift
+//  RightNow
+//
+//  Created by Nikhil Sarin on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct RightNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RightNowTests/RightNowTests.swift
@@ -0,0 +1,16 @@
+//
+//  RightNowTests.swift
+//  RightNowTests
+//
+//  Created by Nikhil Sarin on 4/12/25.
+//
+
+import Testing
+
+struct RightNowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
